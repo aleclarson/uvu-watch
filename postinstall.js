fb3fb6728a1400a302766c725a583d4c7b3f93e3
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-03-23 23:26:12 -0400

    fix: check if dir exists

--- postinstall.js
+++ postinstall.js
@@ -3,12 +3,17 @@ const fs = require('fs')
 
 if (process.cwd().includes('uvu-watch')) {
   process.chdir('../..')
-} else {
-  fs.mkdirSync('node_modules')
 }
 
-fs.mkdirSync('node_modules/.bin')
+mkdirp('node_modules/.bin')
 fs.symlinkSync(
   '../uvu-watch/dist/uvu-watch.js',
   'node_modules/.bin/uvu'
 )
+
+function mkdirp(dir) {
+  if (!fs.existsSync(dir)) {
+    mkdirp(path.dirname(dir))
+    fs.mkdirSync(dir)
+  }
+}
