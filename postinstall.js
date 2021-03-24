2ba4c0397aea940f787d542ea646c066a1533fec
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-03-23 23:22:50 -0400

    fix: use sync fs

--- postinstall.js
+++ postinstall.js
@@ -4,10 +4,10 @@ const fs = require('fs')
 if (process.cwd().includes('uvu-watch')) {
   process.chdir('../..')
 } else {
-  fs.mkdir('node_modules')
+  fs.mkdirSync('node_modules')
 }
 
-fs.mkdir('node_modules/.bin')
+fs.mkdirSync('node_modules/.bin')
 fs.symlinkSync(
   '../uvu-watch/dist/uvu-watch.js',
   'node_modules/.bin/uvu'
